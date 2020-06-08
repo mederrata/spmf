12ceba95b9a538d88508a67ed5f91246aab328b9
Chang <changjc@CC4RM2D867L01.fios-router.home>
2020-06-08 13:37:41 -0400

    update csv script

--- bin/factorize_csv.py
+++ bin/factorize_csv.py
@@ -4,6 +4,8 @@ import sys
 import argparse
 import csv
 import itertools
+import pandas as pd
+import arviz as az
 
 import numpy as np
 import tensorflow as tf
@@ -22,8 +24,8 @@ def main():
         help="Enter the CSV file"
     )
     parser.add_argument(
-        '-e', '--epoch', nargs='?', type=int, default=250,
-        help='Enter Epoch value: Default: 250'
+        '-e', '--epoch', nargs='?', type=int, default=2,
+        help='Enter Epoch value: Default: 300'
     )
     parser.add_argument(
         '-d', '--dimension', nargs='?', type=int, default=2,
@@ -44,7 +46,14 @@ def main():
         help='Log-transform?', action='store_true'
     )
 
+    parser.add_argument(
+        '-rn', '--row-normalize',
+        help='Row normalize based on counts?', action='store_true'
+    )
+
     args = parser.parse_args(sys.argv[1:])
+    args.csv_file = "/Users/changjc/test.csv"
+    args.row_normalize = True
     if args.csv_file is None:
         sys.exit("You need to specify a csv file")
     elif not os.path.exists(args.csv_file):
@@ -57,23 +66,49 @@ def main():
     _EPOCH_NUMBER = args.epoch
     _DIMENSION = args.dimension
     _LEARNING_RATE = args.learning_rate
+    _ROW_NORMALIZE = args.row_normalize
 
     with open(_FILENAME) as f:
         csv_file = csv.reader(f)
         columns = len(next(csv_file))
 
-    N = sum(1 for line in open(_FILENAME))
+    csv_data0 = tf.data.experimental.CsvDataset(
+        _FILENAME, [tf.float64]*columns)
+    csv_data0 = csv_data0.enumerate()
 
-    csv_data = tf.data.experimental.CsvDataset(_FILENAME, [tf.float64]*columns)
-    csv_data = csv_data.enumerate()
-    csv_data = csv_data.map(
-        lambda j, *x: {'indices': j, 'data': tf.squeeze(tf.stack(x, axis=-1))})
+    csv_data = csv_data0.map(
+        lambda j, *x: {
+            'indices': j,
+            'data': tf.squeeze(tf.stack(x, axis=-1))
+        })
+
+    # Grab a batch to compute statistics
+    colsums = []
+    batch_sizes = []
+    N = 0
+    for batch in iter(csv_data.batch(_BATCH_SIZE, drop_remainder=False)):
+        colsums += [tf.reduce_sum(batch['data'], axis=0, keepdims=True)]
+        N += batch['data'].shape[0]
+
+    colsums = tf.add_n(colsums)
+    colmeans = colsums/N
+    rowmean = tf.reduce_sum(colmeans)
+
+    if _ROW_NORMALIZE:
+        csv_data = csv_data0.map(
+            lambda j, *x: {
+                'indices': j,
+                'data': tf.squeeze(tf.stack(x, axis=-1)),
+                'normalization': tf.reduce_max([
+                    tf.reduce_sum(x), 1.])/rowmean
+            })
 
     csv_data_batched = csv_data.batch(_BATCH_SIZE, drop_remainder=True)
 
     factor = PoissonMatrixFactorization(
         csv_data_batched, latent_dim=_DIMENSION, strategy=None,
         scale_columns=True, log_transform=_LOG_TRANSFORM,
+        column_norms=colmeans,
         u_tau_scale=1.0/np.sqrt(columns*N),
         dtype=tf.float64)
 
@@ -84,12 +119,20 @@ def main():
 
     print("Saving the encoding matrix")
 
-    filename = f"{_FILENAME}_{_DIMENSION}D_encoding_{_LOG_TRANSFORM}.csv"
+    filename = f"{_FILENAME}_{_DIMENSION}D_encoding"
+    filename += f"_lt_{_LOG_TRANSFORM}_rn_{_ROW_NORMALIZE}.csv"
     with open(filename, "w") as f:
         writer = csv.writer(f)
         encoding = factor.encoding_matrix().numpy().T
         for row in range(encoding.shape[0]):
             writer.writerow(encoding[row, :])
+
+    print("Saving the trained model object")
+    filename = f"{_FILENAME}_{_DIMENSION}D_model"
+    filename += f"_lt_{_LOG_TRANSFORM}_rn_{_ROW_NORMALIZE}.pkl"
+    factor.save(filename)
+
+    print("Saving figure with the encodings")
 
     fig, ax = plt.subplots(1, 2, figsize=(14, 8))
     D = factor.feature_dim
@@ -105,17 +148,23 @@ def main():
 
     surrogate_samples = factor.surrogate_distribution.sample(250)
     if 's' in surrogate_samples.keys():
-        weights = surrogate_samples['s']/tf.reduce_sum(surrogate_samples['s'],-2,keepdims=True)
+        weights = surrogate_samples['s'] / \
+            tf.reduce_sum(surrogate_samples['s'], -2, keepdims=True)
         intercept_data = az.convert_to_inference_data(
             {
-                r"": 
-                    (tf.squeeze(surrogate_samples['w'])*weights[:,-1,:]*factor.column_norm_factor).numpy().T})
+                r"":
+                    (
+                        tf.squeeze(surrogate_samples['w'])
+                        * weights[:, -1, :]
+                        * factor.column_norm_factor
+                    ).numpy().T})
     else:
         intercept_data = az.convert_to_inference_data(
             {
-                r"": 
-                    (tf.squeeze(surrogate_samples['w'])*factor.column_norm_factor).numpy().T})    
-
+                r"":
+                    (
+                        tf.squeeze(surrogate_samples['w'])
+                        * factor.column_norm_factor).numpy().T})
 
     fig.colorbar(pcm, ax=ax[0], orientation="vertical")
     az.plot_forest(intercept_data, ax=ax[1])
@@ -123,8 +172,26 @@ def main():
     ax[1].set_ylim((-0.014, .466))
     ax[1].set_title("65% and 95% CI")
     ax[1].axvline(1.0, linestyle='dashed', color="black")
-    plt.savefig(f"{_FILENAME}_{_DIMENSION}D_encoding_{_LOG_TRANSFORM}.pdf", bbox_inches='tight')
-    # plt.show()
+    filename = f"{_FILENAME}_{_DIMENSION}D_encoding_"
+    filename += f"lt_{_LOG_TRANSFORM}_rn_{_ROW_NORMALIZE}.pdf"
+    plt.savefig(
+        filename,
+        bbox_inches='tight')
+
+    print("Generating representations")
+    filename = f"{_FILENAME}_{_DIMENSION}D_representation"
+    filename += f"_lt_{_LOG_TRANSFORM}_rn_{_ROW_NORMALIZE}.csv"
+
+    csv_data_batched = csv_data.batch(_BATCH_SIZE, drop_remainder=False)
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        for record in iter(csv_data_batched):
+            z = factor.encode(tf.cast(record['data'], factor.dtype)).numpy()
+            if _ROW_NORMALIZE:
+                z *= (record['normalization'].numpy())[:, np.newaxis]
+            ind = record['indices'].numpy()
+            for row in range(z.shape[0]):
+                writer.writerow(np.concatenate([[ind[row]], z[row, :]]))
 
 
 if __name__ == "__main__":
