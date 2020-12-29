c2e46de6fcc62e1d763b29677de4a22a85d99d9e
hungrymonkey <771339+hungrymonkey@users.noreply.github.com>
2020-12-29 12:57:23 -0800

    bump acceptable pandas versions
    Modin 0.83 allows pandas 1.1.5

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setuptools.setup(
         'dill>=0.3.1.1',
         'matplotlib>=3.1',
         'numpy>=1.17',
-        'pandas>=1.0.0, <1.1.0',
+        'pandas>=1.0.0, <1.2.0',
         'scipy>=1.4.1', #We need to check for direct depends or we can delete scipy.
         'tensorflow>=2.4.0,
         'tensorflow-probability>=0.12.1',
