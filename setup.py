24c7a93abbdc17f6f59c6daa084f8bc29c3ef652
Josh Chang <joshchang@ucla.edu>
2020-12-29 13:22:44 -0500

    update beta prior

--- setup.py
+++ setup.py
@@ -40,8 +40,8 @@ setuptools.setup(
         'numpy>=1.17',
         'pandas>=1.0.0, <1.1.0',
         'scipy>=1.4.1', #We need to check for direct depends or we can delete scipy.
-        'tensorflow>=2.3.0, <2.4.0',
-        'tensorflow-probability>=0.11.0',
+        'tensorflow>=2.4.0,
+        'tensorflow-probability>=0.11.1',
         'tensorflow-addons>=0.11.0'
         ]
 )
